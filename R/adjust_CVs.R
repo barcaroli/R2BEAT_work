c98a6b1dd2ec1ff61ff72e48921041192d45b277
Giulio Barcaroli <gbarcaroli@gmail.com>
2024-04-01 10:15:22 +0200

    Update adjust_CVs.R

--- R/adjust_CVs.R
+++ R/adjust_CVs.R
@@ -2,23 +2,25 @@
 #'  
 #' @description
 #' Function to increase or decrease the precision constraints in order to obtain the desired sample size 
-#' 
+#'  
 #' 
 #' @param target_size desired sample size.
 #' @param strata the 'strata' dataset.
-#' @param errors the 'errors' dataset containing the current precision constraints.
+#' @param errors the 'errors' dataset containing the current precision constraints
+#' @param adj_rate the rate of adjustment (default=0.01): 
+#' the smaller, the higher the precision in reaching the target size; the higher, the quicker is the adjustment
 #' 
-#' @return the new 'errors' dataset containing the changed precision constraints
+#' @return the new 'errors' dataset containing the modified precision constraints
 #' 
 #' @examples
 #' data(beat.example)
 #' errors
 #' a <- beat.1st(strata,errors)
 #' sum(a$alloc$ALLOC[-nrow(a$alloc)])
-#' errors_new <- adjust_CVs(9000,strata,errors)
+#' errors_new <- adjust_CVs(9000,strata,errors,adj_rate=0.005)
 #' errors_new
 
-adjust_CVs <- function(target_size,strata,errors) {
+adjust_CVs <- function(target_size,strata,errors,adj_rate=0.01) {
   a <- beat.1st(stratif=strata,errors=errors)
   cvnew <- errors
   size <- sum(a$alloc$ALLOC[-nrow(a$alloc)])
@@ -28,7 +30,6 @@ adjust_CVs <- function(target_size,strata,errors) {
         cvnew[,k] <- cvnew[,k] - 0.01*cvnew[,k]
       }
       b <- beat.1st(stratif=strata,errors=cvnew)
-      # cat("\n Size: ",sum(b$alloc$ALLOC[-nrow(b$alloc)]))
       if (sum(b$alloc$ALLOC[-nrow(b$alloc)]) > target_size) break
     } 
   }
@@ -38,7 +39,6 @@ adjust_CVs <- function(target_size,strata,errors) {
         cvnew[,k] <- cvnew[,k] + 0.01*cvnew[,k]
       }
       b <- beat.1st(stratif=strata,errors=cvnew)
-      # cat("\n Size: ",sum(b$alloc$ALLOC[-nrow(b$alloc)]))
       if (sum(b$alloc$ALLOC[-nrow(b$alloc)]) < target_size) break
     } 
   }
